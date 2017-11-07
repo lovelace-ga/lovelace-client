6eb1593a0f4ab1d5e214f06081c5ae4390f34b39
dianamcnulty <dianamcnulty@gmail.com>
2017-11-07 11:36:40 -0500

    change handlebar to handle no sites
    -added if within handlebar
    -removed if from event logic

--- assets/scripts/landingpage/events.js
+++ assets/scripts/landingpage/events.js
@@ -3,13 +3,10 @@ const siteAjax = require('../AJAX/siteajax')
 const siteListTemplate = require('../templates/sitelist.handlebars')
 
 const getSitesSuccess = function (data) {
-  if (data.sites.length > 0) {
-    const showSiteList = siteListTemplate({ sites: data.sites })
-    $('#site-list').html(showSiteList)
-  } else {
-    console.log('we have no customers!!')
-  }
+  const showSiteList = siteListTemplate({ sites: data.sites })
+  $('#site-list').html(showSiteList)
 }
+
 const listAllSites = function () {
   // event.preventDefault() // I think this isn't needed
   siteAjax.getAllSites()
