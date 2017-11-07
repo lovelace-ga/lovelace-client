c1ff0f361d1f6db5dce5514776ee348184cc426e
cavallaroc9 <cavallaroc9@gmail.com>
2017-11-06 21:48:31 -0500

    Add comments to authui for what to hide and show
    Added comments as a reminder of what to hide or show when a certain
    auth call is successful or failed.
    We will need to go back and update this.

--- assets/scripts/auth/authui.js
+++ assets/scripts/auth/authui.js
@@ -10,6 +10,7 @@ const signUpSuccess = function (data) {
 
 const signUpFailure = function (error) {
   console.error('signUpFailure error is', error)
+  // show failure message to user
 }
 
 const signInSuccess = function (data) {
@@ -17,29 +18,40 @@ const signInSuccess = function (data) {
   $('#sign-in input:text').val(null)
   $('#sign-in input:password').val(null)
   store.user = data.user
+  // hide Landing Page
+  // show Dashboard
+  // show success message to user
 }
 
 const signInFailure = function (error) {
   console.log('signInFailure error is', error)
+  // show failure message to user
 }
 
 const signOutSuccess = function () {
   console.log('Signed Out successfully!')
   store.user = null
   console.log(store.user)
+  // Hide logout link
+  // Hide Dashboard
+  // Show Landing Page
+  // Show success message to user
 }
 
 const signOutFailure = function (error) {
   console.log('signOutFailure error is', error)
+  // Show failure message to user
 }
 
 const changePasswordSuccess = function () {
   console.log('Changed Password successfully!')
   $('#change-password input').val(null)
+  // Show success message to user
 }
 
 const changePasswordFailure = function (error) {
   console.log('changePasswordFailure error is', error)
+  // show failure message to user
 }
 module.exports = {
   signUpSuccess,
