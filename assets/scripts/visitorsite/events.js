6247bd7ce4ce27baa8d8c441c58c6760b258816d
dianamcnulty <dianamcnulty@gmail.com>
2017-11-07 16:15:20 -0500

    add read more/less functionality in blog
    adds click handlers for 'read more' and 'read less'
    - read more makes call for site and loops through array of posts to find
    the one with id matching the read more link data-id
    then updates content in sibling element.
    - read less takes content in sibling element and trims it to 150
    characters.

--- assets/scripts/visitorsite/events.js
+++ assets/scripts/visitorsite/events.js
@@ -8,7 +8,6 @@ const publicPostsTemplate = require('../templates/publicblog.handlebars')
 const loadSite = function (siteId) {
   siteAjax.getOneSite(siteId)
     .then((siteData) => {
-      console.log('siteData is', siteData)
       $('.navbar-brand').text(siteData.site.name)
       store.site = siteData.site
       return siteData.site
@@ -16,7 +15,7 @@ const loadSite = function (siteId) {
     .then((data) => {
       const blogArray = data.blog
       blogArray.forEach((item) => {
-        item['content'] = item['content'].substring(0, 50) // trims the content before sending to handlebars
+        item['content'] = item['content'].substring(0, 150) // trims the content before sending to handlebars
       })
       const showPostList = publicPostsTemplate({ posts: data.blog })
       $('#public-posts').html(showPostList)
@@ -24,36 +23,42 @@ const loadSite = function (siteId) {
       $('.read-less').hide()
     })
 
-    .catch(() => console.log('site data not returned'))
+    .catch(() => console.log('site data not returned')) // need error handling
 }
 
 const readMore = function (event) {
   let newContent = ''
   const postId = event.target.dataset.id
   const siteId = store.site.id
-  console.log('siteid is ', siteId)
   siteAjax.getOneSite(siteId)
     .then((siteData) => {
-      console.log('siteData readmore is', siteData)
       return siteData.site
     })
     .then((data) => {
       const blogArray = data.blog
       blogArray.forEach((post) => {
-        if (post.id === postId) {
+        if (post.id === postId) { // finds post with id matching data-id from read more button
           newContent = post.content
         }
       })
-      console.log('newContent is', newContent)
       return newContent
     })
-    .then((newContent) => $(event.target).prev().text(newContent))
-  $(event.target).prev().text(newContent)
+    .then((newContent) => {
+      $(event.target).prev().text(newContent)
+      $(event.target).next().show() // shows 'read less' link
+      $(event.target).hide() // hides 'read more' link
+    })
 }
 
+const readLess = function (event) {
+  const newContent = $(event.target).prev().prev().text().substring(0, 150)
+  $(event.target).prev().prev().text(newContent)
+  $(event.target).prev().show() // shows 'read more' link
+  $(event.target).hide() // hides 'read less' link
+}
 const addHandlers = function () {
   $(document).on('click', '.read-more', readMore)
-  // $(document).on('click', '.read-less', readLess)
+  $(document).on('click', '.read-less', readLess)
 }
 module.exports = {
   loadSite,
